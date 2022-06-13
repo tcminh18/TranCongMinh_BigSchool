83349372c31458e87258b68a860170e7e027b4aa
Minh Trần <trancongminhrat@gmail.com>
2022-06-13 08:01:31 +0700

    bat dau  lam bai ngay 13

--- TranCongMinh_BigSchool/Models/ApplicationDbContext.cs
+++ TranCongMinh_BigSchool/Models/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace TranCongMinh_BigSchool.Models
+    //bat dau lam bai ngay 13
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Category> Category 
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
--- TranCongMinh_BigSchool/Models/Course.cs
+++ TranCongMinh_BigSchool/Models/Course.cs
@@ -19,6 +19,14 @@ namespace TranCongMinh_BigSchool.Models
         [Required]
         public byte CategoryId { get; set; }
 
+
 }
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }                          
+    }
     
 }
